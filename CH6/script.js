058c19e8e171bcd9bb62229d3b1a0620adc284a8
saifShaikh404 <132943658+saifShaikh404@users.noreply.github.com>
2023-10-15 19:25:35 +0530

    Add files via upload

---
+++ CH6/script.js
@@ -0,0 +1,16 @@
+let form = document.querySelector('.myForm')
+// RegEx
+let passCondition = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$"; // checks atleast 1 capital, small and number
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    // console.log(form.email.value, form.password.value) // Accessing value by name
+    // console.log(form.emailId.value, form.pass.value) // Accessing value by id
+    let pass = form.password.value.match(passCondition)
+    if(pass){
+        console.log("condition is right")
+    } else {
+        console.log("condition is wrong")
+    }
+})
